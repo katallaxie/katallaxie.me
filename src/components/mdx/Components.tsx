7e1b6f3a5d0a9c6990cea6a25019cb5a01ed8b86
Sebastian Döll <sebastian@katallaxie.me>
2021-03-15 12:37:24 +0100

    wip: markdown links

--- src/components/mdx/Components.tsx
+++ src/components/mdx/Components.tsx
@@ -3,11 +3,13 @@ import DefaultLayout from '@components/layout/PostLayout'
 import Paragraph from './Paragraph'
 import { H1, H2, H3 } from './Heading'
 import CodeBlock from './CodeBlock'
+import Link from './Link'
 
 export const MdxComponents: MdxRenderComponents = {
   DefaultLayout,
   p: Paragraph,
   code: CodeBlock,
+  a: Link,
   h1: H1,
   h2: H2,
   h3: H3
---
+++ src/components/mdx/Link.tsx
@@ -0,0 +1,28 @@
+import React, { forwardRef } from 'react'
+import clsx from 'clsx'
+
+export interface LinkProps {
+  children?: React.ReactNode
+  className?: string
+}
+
+export const Link = forwardRef<HTMLAnchorElement>(
+  ({ children, className, ...props }: LinkProps, ref) => {
+    const styles = clsx(
+      ['border-b-2', 'border-yellow-300', 'not-italic'],
+      [className]
+    )
+
+    return (
+      <a ref={ref} className={styles} {...props}>
+        {children}
+      </a>
+    )
+  }
+)
+
+if (process.env.NODE_ENV === 'development') {
+  Link.displayName = 'Link'
+}
+
+export default Link
